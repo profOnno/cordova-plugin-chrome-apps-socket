3e87be1668303e5362635a2d7068f082871165a7
Menno Vossen <nemno@kuopzi.nl>
2016-08-10 22:54:40 +0200

    hello

--- src/browser/ChromeSocketProxy.js
+++ src/browser/ChromeSocketProxy.js
@@ -2,18 +2,39 @@
  * aarg hope this will work
  */
 
-
+var ws = null;
 var ChromeSocket = {
 
     create: function (success, error, options) {
+        
+        function postit(callback) {
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function() {
+                if (xhttp.readyState == 4 && xhttp.status == 200) { 
+                    callback(xhttp.responseText);
+                }
+            };
+
+            xhttp.open("POST", "http://localhost:8001/", true); // TODO add cors for 127.0.0.1?
+            xhttp.setRequestHeader("Content-type", "application/json");
+            xhttp.send(JSON.stringify({ action: "create" }));
+        };
+
         console.log("create called");
         console.log(JSON.stringify(options));
-        // start proxy ws-socket
-        // connect to proxy
-        var socketInfo = { "socketId": "bliebla" };
-        // nextline doesn't seem to work???
-//        return success(socketInfo); //run callback
-        return success("bliebla"); //run callback
+        postit(function (res){
+            return success(res)
+        });
+
+            // start proxy ws-socket
+            // connect to proxy
+            //var socketInfo = { "socketId": "bliebla" };
+            // nextline doesn't seem to work???
+    //        return success(socketInfo); //run callback
+
+        }
+
     },
 
     connect: function (success, error, options) {
